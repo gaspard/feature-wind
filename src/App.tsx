f4cb95d2d5fb31411d3fd9d83019ae360cb716ab
Gaspard Bucher <gaspard@lucidogen.com>
2024-05-30 14:47:58 +0200

    Vite + React + TypeScript + TailwindCSS template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="p-8 rounded-md border-teal-300 border-2 bg-amber-500">
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+      </div>
+    </>
+  )
+}
